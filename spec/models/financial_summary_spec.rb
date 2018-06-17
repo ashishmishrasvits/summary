e9bd13556a259d099f068f31e78641d0cce1308a
Ashish Mishra <ashish.mishra@searshc.com>
2018-06-16 22:42:56 -0500

    Ashish - Added more specs for financial summary including currency conversion for total amount

--- spec/models/financial_summary_spec.rb
+++ spec/models/financial_summary_spec.rb
@@ -19,6 +19,36 @@ describe FinancialSummary do
     expect(subject.amount(:deposit)).to eq(Money.from_amount(12.12, :usd))
   end
 
+  it "mutliple format transaction amount  summed up to requested currency" do
+    user = create(:user)
+
+    Timecop.freeze(Time.now) do
+      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
+      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :cad))
+    end
+    subject = FinancialSummary.one_day(user: user, currency: :cad)
+    expect(subject.count(:deposit)).to eq(2)
+    expect(subject.amount(:deposit)).to eq(Money.from_amount(12.64, :cad))
+  end
+  
+  it 'summarizes over one day for currency type :cad' do
+
+    user = create(:user)
+
+    Timecop.freeze(Time.now) do
+      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
+      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :usd))
+    end
+
+    Timecop.freeze(2.days.ago) do
+      create(:transaction, user: user, category: :deposit)
+    end
+
+    subject = FinancialSummary.one_day(user: user, currency: :cad)
+    expect(subject.count(:deposit)).to eq(2)
+    expect(subject.amount(:deposit)).to eq(Money.from_amount(15.09, :cad))
+  end
+
   it 'summarizes over seven days' do
 
     user = create(:user)
@@ -43,7 +73,11 @@ describe FinancialSummary do
 
     Timecop.freeze(30.days.ago) do
       create(:transaction, user: user, category: :deposit, amount: Money.from_amount(2.12, :usd))
-      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(10, :usd))
+      create(:transaction, user: user, category: :deposit, amount: Money.from_amount(40, :usd))
+      create(:transaction, user: user, category: :refund, amount: Money.from_amount(10, :usd))
+      create(:transaction, user: user, category: :refund, amount: Money.from_amount(10, :usd))
+      create(:transaction, user: user, category: :withdraw, amount: Money.from_amount(10, :usd))
+      
     end
 
     Timecop.freeze(8.days.ago) do
@@ -52,6 +86,10 @@ describe FinancialSummary do
 
     subject = FinancialSummary.lifetime(user: user, currency: :usd)
     expect(subject.count(:deposit)).to eq(3)
-    expect(subject.amount(:deposit)).to eq(Money.from_amount(13.12, :usd))
+    expect(subject.count(:withdraw)).to eq(1)
+    expect(subject.count(:refund)).to eq(2)
+    expect(subject.amount(:deposit)).to eq(Money.from_amount(43.12, :usd))
+    expect(subject.amount(:refund)).to eq(Money.from_amount(20, :usd))
+    expect(subject.amount(:withdraw)).to eq(Money.from_amount(10, :usd))
   end
 end
